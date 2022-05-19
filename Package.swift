ee3b8047a7f728c233c9c80abae4a59e46a557df
Unseelie <emunseelie@gmail.com>
2022-05-19 12:40:19 -0500

    include platform .iOS

--- Package.swift
+++ Package.swift
@@ -1,11 +1,13 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KeychainMacOS",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15),
+                      .iOS(.v15)
+                     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
