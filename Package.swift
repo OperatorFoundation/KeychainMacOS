98c10ce53afaa649781aabdd224269119adfa243
consuelita <adelita@operatorfoundation.org>
2024-02-18 13:54:37 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-crypto", from: "3.2.0"),
-        .package(url: "https://github.com/OperatorFoundation/KeychainTypes", from: "1.0.0"),
+        .package(url: "https://github.com/OperatorFoundation/KeychainTypes", from: "1.0.1"),
     ],
     targets: [
         .target(
